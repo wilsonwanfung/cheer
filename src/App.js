54c7f3ca337a94fbb8bc4b5f0212b7c47be87ecd
Wilson Cheung <wilsoncheung@Wilsons-MacBook-Air-2.local>
2024-08-01 21:00:33 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignLanguageGuide from './SignLanguageGuide';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SignLanguageGuide />
+    </div>
+  );
+}
+
+export default App;
